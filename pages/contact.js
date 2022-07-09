c0bd7f5fe9439ad5c6c45f15ff62740dd5f19d8d
chinedum onyema <onyemachinedum@gmail.com>
2022-07-09 23:00:14 +0100

    feat: add google map, modify contact addr

--- pages/contact.js
+++ pages/contact.js
@@ -24,7 +24,7 @@ const Contact = ({ contact, map }) => {
 
       <section className="section" id="contact">
         <div className="container-fluid container-lg">
-          <div className="row row-cols-1 row-cols-md-2 gy-5 py-5">
+          <div className="row row-cols-1 row-cols-md-2 gy-5 py-5 lh-lg">
             <div className="col">
               <ContactInfo {...contact} />
             </div>
@@ -40,7 +40,11 @@ const Contact = ({ contact, map }) => {
 
 Contact.defaultProps = {
   contact: {
-    address: "Plot 62, Bala Kona Street, Kado District, FCT Abuja",
+    // address: "Plot 62, Bala Kona Street, Kado District, FCT Abuja",
+    address: {
+      line1: "Plot 62, Bala Kona Street, ",
+      line2: "Kado District, FCT Abuja."
+    },
     email: "contact@stellaebamconsulting.com",
     phones: ["+2348033533945", "+2347084111505"],
     openHours: [
@@ -60,7 +64,8 @@ Contact.defaultProps = {
   },
   map: {
     link: "https://goo.gl/maps/A6nPRbfiwWJkUQyn8",
-    img: mapImg,
+    mapSrc: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3939.769703887336!2d7.435644690225868!3d9.084732447946934!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x104e753477dc2609%3A0xa1a471b8aea6bc66!2s62%20Bala%20Kona%20St%2C%20Kado%20900108%2C%20Abuja%2C%20Nigeria!5e0!3m2!1sen!2suk!4v1645443236061!5m2!1sen!2suk",
+    imgSrc: mapImg,
   }
 }
 
